b8d2da1c63f1bbe00ac1ec7de5cba70303d53e87
nachi <scarl.natsu729@gmail.com>
2025-02-03 21:59:09 +0900

    exif情報を失くさないよう

--- scrape.go
+++ scrape.go
@@ -3,9 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"image"
-	"image/jpeg"
-	"io"
 	"io/fs"
 	"log"
 	"os"
@@ -13,25 +10,30 @@ import (
 	"strings"
 )
 
-var dir string = ""
-var hook_word string = ""
+var dir string
+var hook_word string
 
 func main() {
+	// コマンドライン引数を明示
+	dirPtr := flag.String("dir", "", "対象ディレクトリのパス")
+	hookWordPtr := flag.String("word", "", "削除するフックワード")
+
 	// 入力をパース
 	flag.Parse()
 
-	// 対象ディレクトリのパスを引数から取得(string)
-	dir = flag.Arg(0)
+	// 引数が正しくない場合は実行方法を明示
+	if *dirPtr == "" || *hookWordPtr == "" {
+		log.Fatal("使用方法: go run scrape.go -dir=<ディレクトリ> -word=<フックワード>")
+	}
 
-	// 削ぎ落としたいファイル名をセット
-	hook_word = flag.Arg(1)
+	dir = *dirPtr
+	hook_word = *hookWordPtr
 
 	// 対象ディレクトリの中の全ファイルを取得([]fs.DirEntry)
 	files, err := getFiles(dir)
 
 	if err != nil {
 		log.Fatal(err)
-		panic(err)
 	}
 
 	// 取得したファイルからJPEGファイルのみを抽出([]os.FileInfo)
@@ -71,64 +73,24 @@ func extractJpegFiles(files []fs.DirEntry) []fs.DirEntry {
 // used by main()
 func renameFiles(jpeg_files []fs.DirEntry, hook_word string) {
 	for _, jpeg_file := range jpeg_files {
-		// オリジナルのファイル名を保管
 		original_file_name := jpeg_file.Name()
-		// フックワードを含む場合のみ
-		if strings.Contains(original_file_name, hook_word) {
-			// ファイルを画像として読み込み
-			decoded_image, _ := decodeImage(jpeg_file)
 
-			if decoded_image != nil {
-				// リサイズした画像を書き込み
-				encodeImage(decoded_image, original_file_name, hook_word)
+		// フックワードを含む場合のみ処理
+		if strings.Contains(original_file_name, hook_word) {
+			new_file_name := strings.Replace(original_file_name, hook_word, "", 1)
+
+			// ファイル名のみ変更 (画像データは変更しない)
+			oldPath := filepath.Join(dir, original_file_name)
+			newPath := filepath.Join(dir, new_file_name)
+
+			err := os.Rename(oldPath, newPath)
+			if err != nil {
+				log.Printf("failed: %s -> %s, エラー: %v\n", oldPath, newPath, err)
+			} else {
+				fmt.Printf("scraping: %s -> %s\n", original_file_name, new_file_name)
 			}
 		}
-
 	}
 
 	fmt.Println("scrape completed!!")
-}
-
-// used by renameFiles()
-func decodeImage(jpeg_file fs.DirEntry) (image.Image, error) {
-	io_file, err := conversionToReader(jpeg_file)
-	if err != nil {
-		return nil, err
-	}
-
-	decoded_image, _, err := image.Decode(io_file)
-	if err != nil {
-		return nil, err
-	}
-	return decoded_image, nil
-}
-
-// used by renameFiles()
-func encodeImage(decoded_image image.Image, original_file_name string, hook_word string) error {
-	if strings.Contains(original_file_name, hook_word) {
-
-	}
-	new_file_name := strings.Replace(original_file_name, hook_word, "", 1)
-	output, err := os.Create(dir + "/" + new_file_name)
-	if err != nil {
-		return err
-	}
-
-	defer output.Close()
-
-	opts := &jpeg.Options{Quality: 100}
-	if err := jpeg.Encode(output, decoded_image, opts); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// used by decodeImage()
-func conversionToReader(jpeg_file fs.DirEntry) (io.Reader, error) {
-	io_file, err := os.Open(dir + "/" + jpeg_file.Name())
-	if err != nil {
-		return nil, err
-	}
-	return io_file, nil
 }
